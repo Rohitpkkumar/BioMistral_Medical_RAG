7aaa50ada11e81ed2dfc7c1e8475366825182ede
Rohit Kumar <rohitpkkumar10@gmail.com>
2026-02-18 17:13:29 +0530

    Delete guardline.py

--- guardline.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import Literal
-
-class llm_schema(BaseModel):
-    classifier:Literal["normal","illegal","harrasment","data extraction attempt","prompt injection"]
-llm_structured_output = llm.with_structured_output(llm_schema)
